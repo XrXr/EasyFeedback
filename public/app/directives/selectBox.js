f052dc413a171b8cef42a3a3cb43548e24dadee2
XrXr <xrsquared@gmail.com>
2015-03-12 01:06:25 -0400

    Fix indentation in select box directive

--- public/app/directives/selectBox.js
+++ public/app/directives/selectBox.js
@@ -1,29 +1,29 @@
 angular.module("easyFeedback")
 .directive("selectBox", function () {
-  return {
-    restrict: "E",
-    link: link,
-    templateUrl: "app/partials/selectBox.html",
-    scope: {onChange: "&", statusFn: "&", selected: "=", content: "&"}
-  };
-  function link (scope, elem, attrs) {
-    scope.bindProperty = attrs.bindProperty;
-    scope.elems = scope.content();
-    console.log(scope.elems)
-    var selectClass = attrs.selectClass;
-    scope.optionClicked = function (i, old) {
-        var inner = elem.children().children();
-        if (inner[i]) {
-            var old_selected = angular.isDefined(old) ? old : scope.selected;
-            inner[old_selected].classList.remove(selectClass);
-            inner[i].classList.add(selectClass);
-            scope.onChange()(i);
-            scope.selected = i;
-        }
+    return {
+        restrict: "E",
+        link: link,
+        templateUrl: "app/partials/selectBox.html",
+        scope: {onChange: "&", statusFn: "&", selected: "=", content: "&"}
     };
-    scope.$watch("selected", scope.optionClicked);
-    elem.ready(function () {
-        scope.optionClicked(0);
-    });
-  }
+    function link (scope, elem, attrs) {
+        scope.bindProperty = attrs.bindProperty;
+        scope.elems = scope.content();
+        var selectClass = attrs.selectClass;
+        scope.optionClicked = function (i, old) {
+            var inner = elem.children().children();
+            if (inner[i]) {
+                var old_selected =
+                    angular.isDefined(old) ? old : scope.selected;
+                inner[old_selected].classList.remove(selectClass);
+                inner[i].classList.add(selectClass);
+                scope.onChange()(i);
+                scope.selected = i;
+            }
+        };
+        scope.$watch("selected", scope.optionClicked);
+        elem.ready(function () {
+            scope.optionClicked(0);
+        });
+    }
 });
