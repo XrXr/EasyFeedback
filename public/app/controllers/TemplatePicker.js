3de71de87573fda5203356fdaa0b0c9588315ab6
XrXr <xrsquared@gmail.com>
2015-03-17 00:09:07 -0400

    Lay out interface for adding templates

--- public/app/controllers/TemplatePicker.js
+++ public/app/controllers/TemplatePicker.js
@@ -1,6 +1,17 @@
 angular.module("easyFeedback")
 .controller("TemplatePicker", function ($scope, $timeout) {
+    $scope.adding_template = false;
     $scope.on_editor = function (editor) {
         editor.setReadOnly(true);
+
+        $scope.new_template = function () {
+            $scope.adding_template = true;
+            editor.setReadOnly(false);
+        };
+
+        $scope.cancel_adding = function () {
+            $scope.adding_template = false;
+            editor.setReadOnly(true);
+        };
     };
 });
